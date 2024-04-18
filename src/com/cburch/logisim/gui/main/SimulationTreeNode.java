d0af416fd1fde45358f79e24c0db7ac78a30aab0
LukasElenbergas <lukas.elenbergas@gmail.com>
2024-04-18 19:25:25 +0300

    1st batch of fixes found after launching

--- src/com/cburch/logisim/gui/main/SimulationTreeNode.java
+++ src/com/cburch/logisim/gui/main/SimulationTreeNode.java
@@ -10,16 +10,23 @@ import javax.swing.tree.TreeNode;
 import com.cburch.logisim.comp.ComponentFactory;
 
 public abstract class SimulationTreeNode implements TreeNode {
-	public abstract ComponentFactory getComponentFactory();
-	public boolean isCurrentView(SimulationTreeModel model) {
-		return false;
-	}
+    public abstract ComponentFactory getComponentFactory();
 
-	public abstract Enumeration<?> children();
-	public abstract boolean getAllowsChildren();
-	public abstract TreeNode getChildAt(int childIndex);
-	public abstract int getChildCount();
-	public abstract int getIndex(TreeNode node);
-	public abstract TreeNode getParent();
-	public abstract boolean isLeaf();
+    public boolean isCurrentView(SimulationTreeModel model) {
+        return false;
+    }
+
+    public abstract Enumeration<? extends TreeNode> children();
+
+    public abstract boolean getAllowsChildren();
+
+    public abstract TreeNode getChildAt(int childIndex);
+
+    public abstract int getChildCount();
+
+    public abstract int getIndex(TreeNode node);
+
+    public abstract TreeNode getParent();
+
+    public abstract boolean isLeaf();
 }
