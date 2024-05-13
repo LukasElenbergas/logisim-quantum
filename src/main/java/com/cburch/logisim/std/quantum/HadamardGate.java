10288e0c933c94d986b3547fd6e35bcd7f5e7075
LukasElenbergas <lukas.elenbergas@gmail.com>
2024-05-13 18:41:33 +0300

    Simple single qubit gates done

--- src/main/java/com/cburch/logisim/std/quantum/HadamardGate.java
+++ src/main/java/com/cburch/logisim/std/quantum/HadamardGate.java
@@ -1,12 +1,30 @@
 package com.cburch.logisim.std.quantum;
 
+import com.cburch.logisim.data.QuantumValue;
+import com.cburch.logisim.data.Value;
 import com.cburch.logisim.instance.*;
+import com.cburch.logisim.util.Icons;
 
 class HadamardGate extends AbstractQuantumGate {
     public static InstanceFactory FACTORY = new HadamardGate();
 
     private HadamardGate() {
-        super("Hadamard Gate", Strings.getter("hadamardGateComponent"));
+        super("Hadamard Gate",
+                Strings.getter("hadamardGateComponent"),
+                Icons.getIcon("hadamardGate.gif"),
+                "H");
     }
 
+    @Override
+    public void propagate(InstanceState state) {
+        Value in = state.getPort(1);
+
+        if (in.isQuantum()) {
+            Value out = new Value(Value.QUANTUM, new QuantumValue(in.qVal, "H"));
+
+            state.setPort(0, out, 1);
+        } else {
+            state.setPort(0, Value.ERROR, 1);
+        }
+    }
 }
--- src/main/java/com/cburch/logisim/std/quantum/PhaseTGate.java
+++ src/main/java/com/cburch/logisim/std/quantum/PhaseTGate.java
@@ -1,11 +1,30 @@
 package com.cburch.logisim.std.quantum;
 
+import com.cburch.logisim.data.QuantumValue;
+import com.cburch.logisim.data.Value;
 import com.cburch.logisim.instance.*;
+import com.cburch.logisim.util.Icons;
 
 class PhaseTGate extends AbstractQuantumGate {
     public static InstanceFactory FACTORY = new PhaseTGate();
 
     private PhaseTGate() {
-        super("T Gate", Strings.getter("tGateComponent"));
+        super("T Gate",
+                Strings.getter("tGateComponent"),
+                Icons.getIcon("tGate.gif"),
+                "T");
+    }
+
+    @Override
+    public void propagate(InstanceState state) {
+        Value in = state.getPort(1);
+
+        if (in.isQuantum()) {
+            Value out = new Value(Value.QUANTUM, new QuantumValue(in.qVal, "T"));
+
+            state.setPort(0, out, 1);
+        } else {
+            state.setPort(0, Value.ERROR, 1);
+        }
     }
 }
