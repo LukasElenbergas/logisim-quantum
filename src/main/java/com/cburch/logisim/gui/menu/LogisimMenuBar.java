758ae758c4481069da85dbd5335bd9e9d0b449a2
LukasElenbergas <lukas.elenbergas@gmail.com>
2024-05-19 17:56:48 +0300

    There is no help for anyone

--- src/main/java/com/cburch/logisim/gui/menu/LogisimMenuBar.java
+++ src/main/java/com/cburch/logisim/gui/menu/LogisimMenuBar.java
@@ -60,37 +60,33 @@ public class LogisimMenuBar extends JMenuBar {
 			edit.localeChanged();
 			project.localeChanged();
 			simulate.localeChanged();
-			help.localeChanged();
 		}
 	}
 	
-	private JFrame parent;
-	private MyListener listener;
-	private Project proj;
+	private final JFrame parent;
+	private final MyListener listener;
+	private final Project proj;
 	private SimulateListener simulateListener = null;
-	private HashMap<LogisimMenuItem,MenuItem> menuItems
-		= new HashMap<LogisimMenuItem,MenuItem>();
-	private ArrayList<ChangeListener> enableListeners;
+	private final HashMap<LogisimMenuItem,MenuItem> menuItems = new HashMap<>();
+	private final ArrayList<ChangeListener> enableListeners;
 	
-	private MenuFile file;
-	private MenuEdit edit;
-	private MenuProject project;
-	private MenuSimulate simulate;
-	private MenuHelp help;
-	
+	private final MenuFile file;
+	private final MenuEdit edit;
+	private final MenuProject project;
+	private final MenuSimulate simulate;
+
 	public LogisimMenuBar(JFrame parent, Project proj) {
 		this.parent = parent;
 		this.listener = new MyListener();
 		this.proj = proj;
-		this.enableListeners = new ArrayList<ChangeListener>();
+		this.enableListeners = new ArrayList<>();
 		
 		add(file = new MenuFile(this));
 		add(edit = new MenuEdit(this));
 		add(project = new MenuProject(this));
 		add(simulate = new MenuSimulate(this));
 		add(new WindowMenu(parent));
-		add(help = new MenuHelp(this));
-		
+
 		LocaleManager.addLocaleListener(listener);
 		listener.localeChanged();
 	}
